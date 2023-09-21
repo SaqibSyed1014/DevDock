0b157fa92888d2ca370d5fffbe0aa3515a7f2933
Saqib Syed <sa@techscale.io>
2023-09-21 20:45:49 +0500

    designed: client's video thumbnail

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="bg-sky py-10">
-    <div class="container mx-auto">
+    <div class="container mx-auto mb-20">
       <div class="grid grid-rows-2 grid-cols-1 lg:grid-rows-1 lg:grid-cols-2 gap-5 text-primary">
         <p class="text-lg">We are a collaborative team. We do offer solid core values and a intention meant to provide solutions, a
           dream come actual growth for the business.</p>
         <div>
           <p class="text-base">Loved by clients around the world</p>
           <hr>
-          <div class="flex gap-10">
+          <div class="flex flex-wrap gap-10">
             <img src="/public/img/clients/Opendoor.svg" alt="Client">
             <img src="/public/img/clients/AWS.svg" alt="Client">
             <img src="/public/img/clients/UBER_3.svg" alt="Client">
@@ -17,13 +17,23 @@
         </div>
       </div>
     </div>
+    <div class="px-7">
+      <div class="relative rounded-[40px] overflow-hidden">
+        <img src="/public/img/rocket.png" alt="Rocket">
+        <div class="absolute left-10 top-0 flex flex-col justify-between h-full py-10">
+          <button class="rounded-full bg-secondary text-primary  py-2.5 px-11 w-fit font-medium">Get the full effect</button>
+          <h3 class="text-white text-[80px] leading-[80px]">Feel the <br> BuzzZZz.</h3>
+          <div class="flex justify-center items-center rounded-full h-[77px] w-[77px] bg-white cursor-pointer p-3">
+            <span class="i-mdi-play w-8 h-8"></span>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
-<script>
-export default {
-  name: "Testimonials"
-}
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
 </script>
 
 <style scoped>
