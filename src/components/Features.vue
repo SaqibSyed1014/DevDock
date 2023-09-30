10e46c4f46623e26dfc74c28a2548805e3b74071
Saqib Syed <sa@techscale.io>
2023-09-30 09:40:22 +0500

    gsap setup for feature cards

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="container mx-auto">
+  <section class="container mx-auto relative">
     <h3 class="text-center text-5xl font-medium py-[100px] w-full lg:w-3/4 mx-auto">
       4 <span class="text-pink">Reasons</span>, Why You Should Use DevDock to Build Product
     </h3>
 
-    <div class="bg-[#EBFAFE] rounded-[10px] text-center pt-28 pb-14 mb-14">
+    <div class="blue-box bg-[#EBFAFE] rounded-[10px] sticky top-[30px] text-center pt-28 pb-14 mb-14">
       <div class="w-3/4 mx-auto">
         <h4 class="text-primary text-4xl font-medium pb-6">
           We offer <span class="rounded-[100px] bg-primary px-5 text-white uppercase">Customized</span> approach
@@ -14,7 +14,7 @@
       </div>
     </div>
 
-    <div class="bg-[#FFE8BC] rounded-[10px] text-center pt-28 pb-14 mb-14">
+    <div class="blue-box bg-[#FFE8BC] rounded-[10px] sticky top-[54px] text-center pt-28 pb-14 mb-14">
       <div class="w-3/4 mx-auto">
         <h4 class="text-primary text-4xl font-medium pb-6">
           We focus on <span class="rounded-[100px] bg-primary px-5 text-white uppercase">Educating</span> your users
@@ -24,7 +24,7 @@
       </div>
     </div>
 
-    <div class="bg-[#BCDFFF] rounded-[10px] text-center pt-28 pb-14 mb-14">
+    <div class="blue-box bg-[#BCDFFF] rounded-[10px] sticky top-[78px] text-center pt-28 pb-14 mb-14">
       <div class="w-3/4 mx-auto">
         <h4 class="text-primary text-4xl font-medium pb-6">
           We provide <span class="rounded-[100px] bg-primary px-5 text-white uppercase">Innovative</span> solutions
@@ -34,7 +34,7 @@
       </div>
     </div>
 
-    <div class="bg-primary rounded-[10px] text-center pt-28 pb-14 mb-14">
+    <div class="blue-box bg-primary rounded-[10px] sticky top-[92px] text-center pt-28 pb-14 mb-14">
       <div class="w-3/4 mx-auto text-white">
         <h4 class="text-4xl font-medium pb-6">
           Timely Delivery <span class="rounded-[100px] bg-white px-5 text-primary uppercase">&&</span> Cost Efficiency
@@ -46,10 +46,63 @@
   </section>
 </template>
 
-<script>
-export default {
-  name: "Features"
-}
+<script setup>
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {
+  const masterTimeline = gsap.timeline();
+  const elements = document.querySelectorAll('.blue-box')
+  const createTimelineForElement = (element) => {
+    const tl = gsap.timeline({
+      scrollTrigger :{
+        trigger: element,
+        start: 'top top', // Adjust the trigger start position as needed
+        end: 'bottom center', // Adjust the trigger end position as needed
+        scrub: 1, // Enables scrubbing for smooth scrolling
+        markers: true,
+        onUpdate: () => {
+          // Get the ScrollTrigger progress value (0 to 1)
+          // const progress = self.progress;
+          // Calculate the scale factor based on the progress
+          // const scaleFactor = 1 - progress * 0.2; // Adjust the factor as needed
+
+          // Apply the scale transformation to the element
+          tl.to(element, { scale: .95, duration: .3 });
+        },
+      }
+    });
+
+    // Return the timeline for the element
+    return tl;
+  };
+
+
+  elements.forEach(element => {
+    const elementTimeline = createTimelineForElement(element);
+    masterTimeline.add(elementTimeline);
+  })
+  masterTimeline.play();
+
+
+  // Add an event listener to watch for scroll events
+  // Apply the scale transformation to the element
+
+  // ScrollTrigger.create({
+  //   trigger: '.blue-box',
+  //   start: 'top bottom', // Adjust the trigger start position as needed
+  //   end: 'bottom bottom', // Adjust the trigger end position as needed
+  //   scrub: true, // Enables scrubbing for smooth scrolling
+  //   markers: true,
+  //   onUpdate: (self) => {
+  //     const progress = self.progress;
+  //     const scaleFactor = 1 - progress * 0.2; // Adjust the factor as needed
+  //     tl.to(elementToAnimate.value, { scale: scaleFactor });
+  //   },
+  //   toggleClass: 'scrolling-up', // Add a class when scrolling up
+  // });
+});
 </script>
 
 <style scoped>
